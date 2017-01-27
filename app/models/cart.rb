0dd411198faa02489415a2d14699ea96305b37d6
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-27 22:57:03 +0900

    annotate

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_carts_on_user_id  (user_id)
+#
+
 class Cart < ApplicationRecord
   belongs_to :user
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  price       :integer
+#  hidden      :integer
+#  sort_order  :integer
+#  image       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Product < ApplicationRecord
 end
