1abbfe3a50b4e10d5f22084fe2edfd0e73e802c5
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-28 16:53:59 +0900

    未実装の仕様を追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe User, type: :model do
   it '有効なファクトリを持つこと' do
     expect(build(:user)).to be_valid
   end
+
+  it '画像を保存できること'
+  it '画像以外のファイルは保存できないこと'
+  it 'カートを持っていること'
+
+  context 'Admin の場合' do
+    it '最後の管理者は削除できないこと'
+  end
 end
