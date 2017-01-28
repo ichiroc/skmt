c4b44a121ace200f4da743a6b604e665ed011e3d
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-28 16:56:48 +0900

    カートファクトリのスペックを追加

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,12 @@
+# coding: utf-8
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '有効なカートがある' do
+    expect( build :cart ).to be_valid
+  end
+  it 'ユーザーに紐付いていること'
+  it 'ユーザーに紐付いていないで保存するとエラーになること'
+  it 'カートに商品を追加出来ること'
+  it 'カート内の商品の合計金額を計算できること'
 end
