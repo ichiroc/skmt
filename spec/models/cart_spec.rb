be04d46c838101eb149dc055b88584f2c8572c3c
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-30 07:35:02 +0900

    カートのユーザー関連テストを追加

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,11 +2,14 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  it '有効なカートがある' do
-    expect( build :cart ).to be_valid
+  subject(:cart){ build :cart }
+  it { is_expected.to be_valid }
+
+  it 'ユーザーに紐付いていないで保存するとエラーになること' do
+    cart.user = nil
+    cart.valid?
+    expect(cart.errors[:user]).to include 'must exist'
   end
-  it 'ユーザーに紐付いていること'
-  it 'ユーザーに紐付いていないで保存するとエラーになること'
   it 'カートに商品を追加出来ること'
   it 'カート内の商品の合計金額を計算できること'
 end
