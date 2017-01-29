fd6957443bfe5be8972435c2b827aeaffff28dee
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-30 06:21:20 +0900

    CartItem の数量のテストを追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -18,4 +18,5 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+  validates :quantity, numericality: { greater_than: 0 }
 end
--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -4,6 +4,12 @@ require 'rails_helper'
 RSpec.describe CartItem, type: :model do
   subject(:cart_item) { build :cart_item }
   it { is_expected.to be_valid }
+  it '数量は1以上であること' do
+    cart_item.quantity = 0
+    cart_item.valid?
+    expect(cart_item.errors[:quantity]).to include 'must be greater than 0'
+  end
+
   it '商品の合計金額を出せること'
-  it '数量は1以上であること'
+
 end
