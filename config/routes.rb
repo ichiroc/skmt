28ccfa0665e83df66c8cee2237bfb45de56e266e
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-01 18:16:01 +0900

    ルートを product#index に定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'products#index'
   resources :cart_items
-  resources :carts
   resources :products
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
