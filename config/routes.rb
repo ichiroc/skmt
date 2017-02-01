36367f6d7797ebea11b3aab56289d52f25ef7be5
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-02 06:20:43 +0900

    製品管理用の Controller を作った

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :cart_items
   resources :products
+  namespace :admin do
+    resources :products
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
