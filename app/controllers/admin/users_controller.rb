32369be7efcbf8579c419c599c2ea8bcaa89069e
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-05 07:10:05 +0900

    JSON のレスポンスをしないようにした

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,60 +1,48 @@
+# frozen_string_literal: true
 class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :not_permitted, unless: 'current_user.is_admin?'
   before_action :set_admin_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /admin/users
-  # GET /admin/users.json
   def index
     @admin_users = User.all
   end
 
-  # GET /admin/users/1
-  # GET /admin/users/1.json
   def show
   end
 
-  # GET /admin/users/1/edit
   def edit
   end
 
-  # PATCH/PUT /admin/users/1
-  # PATCH/PUT /admin/users/1.json
   def update
     respond_to do |format|
       if @admin_user.update(admin_user_params)
         format.html { redirect_to admin_user_path @admin_user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @admin_user }
       else
         format.html { render :edit }
-        format.json { render json: @admin_user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /admin/users/1
-  # DELETE /admin/users/1.json
   def destroy
     @admin_user.destroy
     respond_to do |format|
       format.html { redirect_to admin_users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_admin_user
-      @admin_user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def admin_user_params
-      permitted_attr = [:name, :email, :zip_code, :address, :is_admin]
-      unless params[:user][:password].blank?
-        permitted_attr << :password
-        permitted_attr << :password_confirmation
-      end
-      params.require(:user).permit(*permitted_attr)
+  def set_admin_user
+    @admin_user = User.find(params[:id])
+  end
+
+  def admin_user_params
+    permitted_attr = [:name, :email, :zip_code, :address, :is_admin]
+    unless params[:user][:password].blank?
+      permitted_attr << :password
+      permitted_attr << :password_confirmation
     end
+    params.require(:user).permit(*permitted_attr)
+  end
 end
