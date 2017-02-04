d7a3279c9d2321729b1168ff86027b454191c5db
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-05 07:03:15 +0900

    商品管理のアクセス制御のスペックを書いた

---
+++ spec/features/admin_product_spec.rb
@@ -0,0 +1,26 @@
+# coding: utf-8
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe :admin_product do
+  feature '商品管理メニューを使う' do
+    scenario '匿名ユーザーが直接URLを開いても使えない' do
+      visit admin_products_path
+      expect(current_path).to eq new_user_session_path
+    end
+
+    scenario '一般ユーザーがログインしてもリダイレクトされる' do
+      user = create :user, password: 'password'
+      sign_in user
+      visit admin_products_path
+      expect(current_path).to eq root_path
+    end
+
+    scenario '管理者ユーザーがログインすれば使える' do
+      admin = create :admin
+      sign_in admin
+      visit admin_products_path
+      expect(current_path).to eq admin_products_path
+    end
+  end
+end
