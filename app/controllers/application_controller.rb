ed31cca674cd8114a10462ab553952a4e9f1c52d
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-05 12:29:03 +0900

    カートを取得可能にした
    ログインしているときはユーザーのカートを、未ログインの場合はセッションカートを返す。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :find_cart
   private
+
+  def find_cart
+    if user_signed_in?
+      current_user.cart
+    else
+      session_cart
+    end
+  end
+
+  def session_cart
+    if session[:cart_id]
+      Cart.find session[:cart_id]
+    else
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+  end
 
   def not_permitted
     redirect_to root_url, alert: 'この操作は許可されていません'
