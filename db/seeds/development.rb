0a5f5134fc391f3e202e1b768019d0bbf9b9f9e5
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-06 05:39:19 +0900

    それっぽい初期データを生成するようにした

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,6 +1,26 @@
-u = User.find_or_initialize_by(name: 'Admin', email: 'admin@admin')
+# frozen_string_literal: true
+u = User.new(name: 'Admin', email: 'admin@admin')
 u.password = 'password'
 u.skip_confirmation!
 u.add_role :admin
 u.save!
 puts 'Default admin user is created.'
+
+10.times do |i|
+  u = User.new(name: "User#{i}", email: "user#{i}@user", password: 'password')
+  u.skip_confirmation!
+  u.save!
+end
+puts 'Sample users are created.'
+
+30.times do
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph,
+    price: [1980, 2980, 980, 580, 480, 8800, 4800].sample,
+    hidden: nil,
+    sort_order: [10, 20, 30, 40, 50].sample,
+    image: File.new("#{Rails.root}/db/seeds/images/#{Random.rand(1..5)}.jpg")
+  )
+end
+puts 'Sample products are created.'
