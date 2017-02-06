56a11e54ec5590fcfdbe44f307efdb9d1dbc0d78
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-06 18:35:04 +0900

    modelに関するローカライズファイルを別ファイルに分けた

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Skmt
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     I18n.default_locale = :ja
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
