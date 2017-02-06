4e56b8f7e3bf37ab9e90cb142e83228418090fce
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-07 06:29:54 +0900

    リファクタリング

--- spec/features/admin_users_spec.rb
+++ spec/features/admin_users_spec.rb
@@ -1,17 +1,19 @@
 # coding: utf-8
-require "rails_helper"
+# frozen_string_literal: true
+require 'rails_helper'
 
 RSpec.describe :admin_user do
-
   feature 'ユーザー管理メニューを使う' do
+    before :each do
+      @user = create :user
+    end
     scenario '匿名ユーザーが直接URLを開いても使えない' do
       visit admin_users_path
       expect(current_path).to eq new_user_session_path
     end
 
     scenario '一般ユーザーがログインしてもリダイレクトされる' do
-      user = create :user, password: 'password'
-      sign_in user
+      sign_in @user
       visit admin_users_path
       expect(current_path).to eq root_path
     end
@@ -21,19 +23,20 @@ RSpec.describe :admin_user do
         admin = create :admin
         sign_in admin
       end
-      scenario '管理メニューを表示できる'  do
+      scenario '管理メニューを表示できる' do
         visit admin_users_path
         expect(current_path).to eq admin_users_path
       end
 
       scenario '編集できる' do
-        create :user
         click_link t('menu.admin.users_link')
         first(:link, 'Edit').click
-        fill_in t('activerecord.attributes.user.name'), with: "ほげ"
+        fill_in t('activerecord.attributes.user.name'), with: 'ほげ'
         click_button t('helpers.submit.update')
         expect(page).to have_content 'User was successfully updated.'
         expect(page).to have_content 'ほげ'
+        @user.reload
+        expect(@user.name).to eq 'ほげ'
       end
     end
   end
