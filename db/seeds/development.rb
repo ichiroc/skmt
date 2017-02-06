1f1198ad647ce5c9b28f5457853931353d38c79f
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-07 05:51:21 +0900

    サンプルの商品をご飯にした

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -15,12 +15,12 @@ puts 'Sample users are created.'
 
 30.times do
   Product.create!(
-    name: Faker::Commerce.product_name,
+    name: "#{Faker::Food.spice} #{Faker::Food.ingredient}",
     description: Faker::Lorem.paragraph,
     price: [1980, 2980, 980, 580, 480, 8800, 4800].sample,
     status: 0,
     sort_order: [10, 20, 30, 40, 50].sample,
-    image: File.new("#{Rails.root}/db/seeds/images/#{Random.rand(1..5)}.jpg")
+    image: File.new("#{Rails.root}/db/seeds/images/#{Random.rand(1..8)}.jpg")
   )
 end
 puts 'Sample products are created.'
