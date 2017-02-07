e699768267b4376b432244a6f2fd9a8570097ea4
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-08 07:37:00 +0900

    product_image_tag の引数を image に変更

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,9 +1,9 @@
 module ProductsHelper
-  def product_image_tag(product, options = {})
-    if product.image.blank?
+  def product_image_tag(image, options = {})
+    if image.blank?
       image_tag '/images/dummy-300x300.png', options
     else
-      image_tag product.image.url, options
+      image_tag image.url, options
     end
   end
 end
