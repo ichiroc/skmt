24732aad78222dd9f3515312b144ed0c489861f1
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-08 06:20:18 +0900

    不要なViewとメソッドを削除

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,19 +1,11 @@
-# coding: utf-8
+# frozen_string_literal: true
 class CartItemsController < ApplicationController
   before_action :set_cart_item, only: [:update, :destroy]
 
-  # GET /cart_items
-  # GET /cart_items.json
   def index
     @cart_items = CartItem.all
   end
 
-  # GET /cart_items/1/edit
-  def edit
-  end
-
-  # POST /cart_items
-  # POST /cart_items.json
   def create
     product = Product.find(params[:product_id])
     @cart_item = find_cart.items.find_or_initialize_by(product: product)
@@ -21,46 +13,36 @@ class CartItemsController < ApplicationController
     respond_to do |format|
       if @cart_item.save
         format.html { redirect_to product, notice: 'Cart item was successfully created.' }
-        format.json { render :show, status: :created, location: @cart_item }
       else
         format.html { redirect_to product, alert: 'Cart item was not created.' }
-        format.json { render json: @cart_item.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /cart_items/1
-  # PATCH/PUT /cart_items/1.json
   def update
     respond_to do |format|
       if @cart_item.update(cart_item_params)
         format.html { redirect_to cart_items_path, notice: 'Cart item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @cart_item }
       else
-        format.html { redirect_to cart_items_path,  notice: 'Cart item was failed.' }
-        format.json { render json: @cart_item.errors, status: :unprocessable_entity }
+        format.html { redirect_to cart_items_path, notice: 'Cart item was failed.' }
       end
     end
   end
 
-  # DELETE /cart_items/1
-  # DELETE /cart_items/1.json
   def destroy
     @cart_item.destroy
     respond_to do |format|
       format.html { redirect_to cart_items_url, notice: 'Cart item was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cart_item
-      @cart_item = CartItem.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def cart_item_params
-      params.require(:cart_item).permit(:cart_id, :product_id, :quantity)
-    end
+  def set_cart_item
+    @cart_item = CartItem.find(params[:id])
+  end
+
+  def cart_item_params
+    params.require(:cart_item).permit(:cart_id, :product_id, :quantity)
+  end
 end
