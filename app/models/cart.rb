8812a7e66469a8cd98d077c2f2e83b3973ab7757
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-08 19:09:17 +0900

    カートで税計算

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # == Schema Information
 #
 # Table name: carts
@@ -16,7 +17,17 @@ class Cart < ApplicationRecord
   belongs_to :user, optional: true
   has_many :items, class_name: CartItem
 
+  TAX_RATE = 0.08
+
   def total
+    tax_excluded_total + tax_amount
+  end
+
+  def tax_amount
+    (tax_excluded_total * TAX_RATE).to_i
+  end
+
+  def tax_excluded_total
     items.map(&:total).inject(:+)
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,4 +1,5 @@
 # coding: utf-8
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
@@ -7,22 +8,35 @@ RSpec.describe Cart, type: :model do
   it { is_expected.to be_valid }
 
   it 'カートに商品を追加出来ること' do
-    expect{
+    expect do
       cart.items << build(:cart_item)
       cart.save
-    }.to change(cart.items, :count).by(+1)
+    end.to change(cart.items, :count).by(+1)
   end
 
-  it 'カート内の商品の合計金額を計算できること' do
-    items = 10.times.map{ build :cart_item }
-    cart.items = items
-    expect(cart.total).to eq items.map(&:total).inject(:+)
-    # 念のため
-    cart.items = []
-    i1 = build :cart_item
-    i2 = build :cart_item
-    cart.items << i1
-    cart.items << i2
-    expect(cart.total).to eq i1.total + i2.total
+  describe '計算' do
+    it 'カート内の商品の合計金額を計算できること' do
+      items = 10.times.map{ build :cart_item }
+      cart.items = items
+      expect(cart.total).to eq (items.map(&:total).inject(:+) * 1.08 ).to_i
+      # 念のため
+      cart.items = []
+      i1 = build :cart_item
+      i2 = build :cart_item
+      cart.items << i1
+      cart.items << i2
+      expect(cart.total).to eq ((i1.total + i2.total) * 1.08).to_i
+    end
+
+    describe '税計算' do
+      it 'カート内の商品の税額を計算できる' do
+        i1 = build :cart_item
+        i2 = build :cart_item
+        cart.items << i1
+        cart.items << i2
+        tax = ((i1.total + i2.total) * 0.08).to_i
+        expect(cart.tax_amount).to eq tax
+      end
+    end
   end
 end
