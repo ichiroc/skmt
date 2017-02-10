a3b78a234ae94ff24d86885f61475fa0257631ef
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-10 18:34:56 +0900

    カート品目から注文品目がコピーされるスペックを追加

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,6 +1,15 @@
+# coding: utf-8
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
   subject(:order_item) { build :order_item }
   it{ is_expected.to be_valid }
+
+  it 'バリデーション前にカート品目から情報が生成される' do
+    order_item.valid?
+    expect(order_item.price).to eq order_item.cart_item.product.price
+    expect(order_item.product_name).to eq order_item.cart_item.product.name
+    expect(order_item.quantity).to eq order_item.quantity
+  end
+
 end
