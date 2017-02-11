0a4d9329121a4351346332e651a9e9322923cb2d
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-11 22:13:27 +0900

    Devise のビューを追加して住所と郵便番号を変更できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
     resources :users, except:[:new, :create]
   end
   devise_for :users, controllers: {
-               sessions: 'users/sessions'
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
              }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
