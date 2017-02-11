c5736311d49088ec6fec6fb3f177d58af4906d54
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-11 19:21:44 +0900

    ファクトリで無効な配達時間帯が指定される場合があるバグを修正

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     tax_amount { Faker::Commerce.price 100..1_000 }
     delivery_fee { 500 * Random.rand(1..10) }
     cache_on_delivery_fee { [300, 400, 600, 1000].sample }
-    delivery_time_zone { Random.rand 0..7 }
+    delivery_time_zone { Random.rand 0..6 }
     delivery_date Time.now
     destination_name { Faker::Name.name }
     destination_zip_code { Faker::Address.zip_code }
