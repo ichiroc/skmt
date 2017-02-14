98df04f49b4390485e66c160a1489d8b50d567e7
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-15 05:26:50 +0900

    不要なJSONファイルは削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,10 +25,8 @@ class OrdersController < ApplicationController
     respond_to do |format|
       if @order.save
         format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -37,10 +35,8 @@ class OrdersController < ApplicationController
     respond_to do |format|
       if @order.update(order_params)
         format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
       else
         format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -49,7 +45,6 @@ class OrdersController < ApplicationController
     @order.destroy
     respond_to do |format|
       format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/views/orders/_order.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! order, :id, :user_id, :total, :tax_amount, :delivery_fee, :cache_on_delivery_fee, :delivery_time_slot, :delivery_date, :destination_name, :destination_zip_code, :destination_address, :created_at, :updated_at
-json.url order_url(order, format: :json)
