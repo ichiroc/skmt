92d94579f90431aa786ba218bdd93cc1ffb217cc
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-14 19:11:13 +0900

    Pundit の scope を商品管理に適用

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,10 +4,11 @@ class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :not_permitted, unless: 'current_user.is_admin?'
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  after_action :verify_authorized
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
 
   def index
-    @products = authorize Product.all.order(:sort_order)
+    @products = policy_scope(Product).order(:sort_order)
   end
 
   def show
