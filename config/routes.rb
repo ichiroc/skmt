3c96413491ceba9f8682a5288ce19edd4cc4437b
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-16 08:08:28 +0900

    使っていないので OrderItem のrouteを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
-  resources :order_items
-  resources :orders
   root 'products#index'
+
+  resources :orders
   resources :cart_items, only: [:index, :create, :update, :destroy]
   resources :products, only: [:show, :index]
   namespace :admin do
     resources :products
-    resources :users, except:[:new, :create]
+    resources :users, except: [:new, :create]
   end
   devise_for :users, controllers: {
-               sessions: 'users/sessions',
-               registrations: 'users/registrations'
-             }
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
