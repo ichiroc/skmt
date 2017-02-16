ce4c3ae74822a3fcf276fea8412b55872075eff8
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-17 07:49:49 +0900

    18n.available_locales にサポートするロケールを追加

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Skmt
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    I18n.available_locales = [:en, :ja]
     I18n.default_locale = :ja
     I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Tokyo'
