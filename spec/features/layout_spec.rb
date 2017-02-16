df6062ae64d24bc968fb042502cc0f905ea394e0
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-17 05:26:56 +0900

    一般ユーザーの場合の管理者メニューのスペックを追加

--- spec/features/layout_spec.rb
+++ spec/features/layout_spec.rb
@@ -3,14 +3,27 @@ require "rails_helper"
 
 RSpec.describe :layout do
   feature '管理者メニュー' do
-    scenario '管理者メニューを表示する' do
-      admin = create :admin
-      visit root_path
-      expect(page).not_to have_content t('menu.manage_products')
-      expect(page).not_to have_content t('menu.manage_users')
-      sign_in admin
-      expect(page).to have_content t('menu.manage_products')
-      expect(page).to have_content t('menu.manage_users')
+    context '一般ユーザーの場合' do
+      let(:user) { create :user }
+      scenario '一般ユーザーで入ると非表示する' do
+        visit root_path
+        expect(page).not_to have_content t('menu.manage_products')
+        expect(page).not_to have_content t('menu.manage_users')
+        sign_in user
+        expect(page).not_to have_content t('menu.manage_products')
+        expect(page).not_to have_content t('menu.manage_users')
+      end
+    end
+    context '管理者ユーザーの場合' do
+      let(:user){ create :admin }
+      scenario '管理者で入ると管理者メニューを表示する' do
+        visit root_path
+        expect(page).not_to have_content t('menu.manage_products')
+        expect(page).not_to have_content t('menu.manage_users')
+        sign_in user
+        expect(page).to have_content t('menu.manage_products')
+        expect(page).to have_content t('menu.manage_users')
+      end
     end
   end
 end
