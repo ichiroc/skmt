89309470f59dc735c9f29857bd9cf7131618e08b
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-17 23:51:32 +0900

    production 環境でのメール送信

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "skmt_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    domain: 'herokuapp.com',
+    authentication: :plain,
+    password: ENV['SENDGRID_PASSWORD'],
+    user_name: ENV['SENDGRID_USERNAME'],
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
