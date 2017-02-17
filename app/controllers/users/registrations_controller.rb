9a3b2dc192f9bf829ed1d8dfcf279381b503f4c6
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-18 00:31:57 +0900

    氏名が変更されないバグを修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,12 +40,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:zip_code, :address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:zip_code, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :zip_code, :address])
   end
 
   # The path used after sign up.
