02d17510d87ee26ea0dd26f0e1084c342f15b7f2
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-17 23:56:36 +0900

    default_url_options に host を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "skmt_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'skmt.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
