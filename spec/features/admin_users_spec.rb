abdb494f2d9eb09544d02776e395e043167e87bf
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-19 07:24:35 +0900

    ユーザー編集のスペックを整理

--- spec/features/admin_users_spec.rb
+++ spec/features/admin_users_spec.rb
@@ -11,31 +11,32 @@ feature 'ユーザー管理メニューを使う' do
     expect(current_path).to eq new_user_session_path
   end
 
-  scenario '一般ユーザーがログインしてもリダイレクトされる' do
+  scenario '一般ユーザーが直接URLを開いてもリダイレクトされる' do
     sign_in @user
     visit admin_users_path
     expect(current_path).to eq root_path
+    expect(page).to have_content t('errors.messages.not_permitted_operation')
   end
 
-  context '管理者ユーザーでログインした場合' do
-    before :each do
-      admin = create :admin
-      sign_in admin
-    end
-    scenario '管理メニューを表示できる' do
-      visit admin_users_path
-      expect(current_path).to eq admin_users_path
-    end
+  scenario '管理者ユーザーでユーザーの情報を編集する' do
+    admin = create :admin
+    name = 'hoge'
+    zip_code = '9999999'
+    address = 'fuga'
 
-    scenario '編集できる' do
-      click_link t('menu.manage_users')
-      first(:link, t('helpers.links.edit')).click
-      fill_in t('activerecord.attributes.user.destination_name'), with: 'ほげ'
-      click_button t('helpers.submit.update')
-      expect(page).to have_content 'User was successfully updated.'
-      expect(page).to have_content 'ほげ'
-      @user.reload
-      expect(@user.destination_name).to eq 'ほげ'
-    end
+    sign_in admin
+    visit admin_users_path
+    click_link t('menu.manage_users')
+    first(:link, t('helpers.links.edit')).click
+    fill_in t('activerecord.attributes.user.destination_name'), with: name
+    fill_in t('activerecord.attributes.user.destination_zip_code'), with: zip_code
+    fill_in t('activerecord.attributes.user.destination_address'), with: address
+    click_button t('helpers.submit.update')
+
+    expect(page).to have_content 'User was successfully updated.'
+    @user.reload
+    expect(@user.destination_name).to eq name
+    expect(@user.destination_zip_code).to eq zip_code
+    expect(@user.destination_address).to eq address
   end
 end
