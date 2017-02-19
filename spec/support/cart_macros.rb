c7e9efdceb649869c515687a4a83b5c40445ff36
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-19 13:03:43 +0900

    ローカライズの影響で失敗していたスペックを修正

--- spec/support/cart_macros.rb
+++ spec/support/cart_macros.rb
@@ -1,11 +1,11 @@
 module CartMacros
   def add_to_cart product
     click_link product.name
-    click_link 'Add to cart'
+    click_link t('products.show.add_to_cart')
   end
 
   def proceed_to_checkout
-    click_on t('menu.cart')
-    click_on 'Proceed To Checkout'
+    visit cart_items_path
+    click_on t('cart_items.index.proceed_to_checkout')
   end
 end
