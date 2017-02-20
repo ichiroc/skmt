201566bc324f42a7b7ef897264d738272d58f115
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-02-20 18:01:48 +0900

    リファクタリング

--- app/validators/zip_code_format_validator.rb
+++ app/validators/zip_code_format_validator.rb
@@ -3,8 +3,7 @@ class ZipCodeFormatValidator < ActiveModel::Validator
   ZIP_CODE_REGEXP = /\A\d{7}\Z/
   def validate(record)
     return if record.destination_zip_code.blank?
-    unless record.destination_zip_code =~ ZIP_CODE_REGEXP
-      record.errors.add(:destination_zip_code, I18n.t('errors.messages.destination_zip_code'))
-    end
+    return if record.destination_zip_code =~ ZIP_CODE_REGEXP
+    record.errors.add(:destination_zip_code, I18n.t('errors.messages.destination_zip_code'))
   end
 end
